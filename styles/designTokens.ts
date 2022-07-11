b647938d6484477bae7baa9c3377c98ae09ea291
András Szesztai <andras.szesztai@nielsen.com>
2022-07-11 18:53:49 +0200

    feat: easlint,tsconfig and global styles setup

---
+++ styles/designTokens.ts
@@ -0,0 +1,39 @@
+export const designTokens = {
+    space: {
+        xxs: 4,
+        xs: 8,
+        sm: 12,
+        base: 16,
+        md: 24,
+        lg: 32,
+        xl: 48,
+        xxl: 64,
+        '3xl': 96,
+        '4xl': 128,
+        '5xl': 192,
+    },
+    strokeWidth: {
+        sm: 1,
+        md: 2,
+        lg: 4,
+    },
+    color: {
+        black: '#000000',
+        background: '#FDF2E4',
+        accentOne: '#F6C15C',
+        accentTwo: '#EE7A50',
+        accentThree: '#F9DAA8',
+    },
+    fontSize: {
+        base: '1rem',
+        md: '1.5rem',
+        lg: '2rem',
+        xl: '3rem',
+        xxl: '4rem',
+    },
+    breakPoints: {
+        sm: 360,
+        md: 720,
+        lg: 1040,
+    },
+} as const
---
+++ styles/global.ts
@@ -0,0 +1,58 @@
+import { css } from '@emotion/react'
+import { designTokens } from './designTokens'
+
+export const globalStyles = css`
+    :root {
+        font-family: 'Share Tech Mono', monospace;
+        font-weight: 400;
+        color: ${designTokens.color.black};
+        font-style: normal;
+    }
+
+    *,
+    *::before,
+    *::after {
+        box-sizing: border-box;
+    }
+
+    * {
+        margin: 0;
+    }
+
+    html,
+    body {
+        height: 100%;
+        background-color: ${designTokens.color.background};
+    }
+
+    body {
+        line-height: 1.5;
+        -webkit-font-smoothing: antialiased;
+    }
+
+    img,
+    picture,
+    video,
+    canvas,
+    svg {
+        display: block;
+        max-width: 100%;
+    }
+
+    input,
+    button,
+    textarea,
+    select {
+        font: inherit;
+    }
+
+    p,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+        overflow-wrap: break-word;
+    }
+`
