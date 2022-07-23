7ec2164446bd73b9ecbd66b4e3b2fe37348aa89f
András Szesztai <andras.szesztai@nielsen.com>
2022-07-23 11:33:56 +0200

    style: responsive OrText and HomeContentContaiere

--- components/atoms/containers/HomeContentContainer/HomeContentContainer.tsx
+++ components/atoms/containers/HomeContentContainer/HomeContentContainer.tsx
@@ -1,12 +1,36 @@
 import styled from '@emotion/styled'
 import { designTokens } from 'styles/designTokens'
 
+const { space, breakPoints } = designTokens
+
+const HomeContainerStylesMapping = {
+    gap: {
+        base: space.lg,
+        [breakPoints.sm]: space.xl,
+        [breakPoints.md]: space.xxl,
+        [breakPoints.lg]: space.xxl,
+    },
+} as const
+
 const HomeContentContainer = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    gap: ${designTokens.space.xxl}px;
+
+    gap: ${HomeContainerStylesMapping.gap.base}px;
+
+    @media only screen and (min-width: ${breakPoints.sm}px) {
+        gap: ${HomeContainerStylesMapping.gap[breakPoints.sm]}px;
+    }
+
+    @media only screen and (min-width: ${breakPoints.md}px) {
+        gap: ${HomeContainerStylesMapping.gap[breakPoints.md]}px;
+    }
+
+    @media only screen and (min-width: ${breakPoints.lg}px) {
+        gap: ${HomeContainerStylesMapping.gap[breakPoints.lg]}px;
+    }
 `
 
 export default HomeContentContainer
--- components/molecules/JoinRoom/styles.ts
+++ components/molecules/JoinRoom/styles.ts
@@ -1,13 +1,37 @@
 import styled from '@emotion/styled'
 import { designTokens } from 'styles/designTokens'
 
+const { fontSize, breakPoints } = designTokens
+
 export const JoinRoomContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
 `
 
+const OrTextStylesMapping = {
+    fontSize: {
+        base: fontSize.base,
+        [breakPoints.sm]: fontSize.md,
+        [breakPoints.md]: fontSize.lg,
+        [breakPoints.lg]: fontSize.lg,
+    },
+} as const
+
 export const OrText = styled.h2`
-    font-size: ${designTokens.fontSize.lg};
     margin-bottom: ${designTokens.space.xxs}px;
+
+    font-size: ${OrTextStylesMapping.fontSize.base};
+
+    @media only screen and (min-width: ${breakPoints.sm}px) {
+        font-size: ${OrTextStylesMapping.fontSize[breakPoints.sm]};
+    }
+
+    @media only screen and (min-width: ${breakPoints.md}px) {
+        font-size: ${OrTextStylesMapping.fontSize[breakPoints.md]};
+    }
+
+    @media only screen and (min-width: ${breakPoints.lg}px) {
+        font-size: ${OrTextStylesMapping.fontSize[breakPoints.lg]};
+    }
 `
