09f5f7b9077516ccf5702610a790878da4a10157
András Szesztai <andras.szesztai@nielsen.com>
2022-07-23 11:28:15 +0200

    style: responsive MainTitle fontsize

--- components/atoms/MainTitle/MainTitle.tsx
+++ components/atoms/MainTitle/MainTitle.tsx
@@ -1,9 +1,33 @@
 import styled from '@emotion/styled'
 import { designTokens } from 'styles/designTokens'
 
+const { fontSize, breakPoints } = designTokens
+
+const MainTitleStylesMapping = {
+    fontSize: {
+        base: fontSize.lg,
+        [breakPoints.sm]: fontSize.xl,
+        [breakPoints.md]: fontSize.xl,
+        [breakPoints.lg]: fontSize.xxl,
+    },
+} as const
+
 const MainTitle = styled.h1`
-    font-size: ${designTokens.fontSize.xxl};
     text-align: center;
+
+    font-size: ${MainTitleStylesMapping.fontSize.base};
+
+    @media only screen and (min-width: ${breakPoints.sm}px) {
+        font-size: ${MainTitleStylesMapping.fontSize[breakPoints.sm]};
+    }
+
+    @media only screen and (min-width: ${breakPoints.md}px) {
+        font-size: ${MainTitleStylesMapping.fontSize[breakPoints.md]};
+    }
+
+    @media only screen and (min-width: ${breakPoints.lg}px) {
+        font-size: ${MainTitleStylesMapping.fontSize[breakPoints.lg]};
+    }
 `
 
 export default MainTitle
