828cb499c5984486d36f912a8a1602c60d79d49d
András Szesztai <andras.szesztai@nielsen.com>
2022-07-24 20:43:55 +0200

    feat: add start button to lobby for admin

--- components/templates/LobbyPageContent/LobbyPageContent.tsx
+++ components/templates/LobbyPageContent/LobbyPageContent.tsx
@@ -1,9 +1,14 @@
+import { SquareLoader } from 'react-spinners'
+import { Role } from '@prisma/client'
+
+import { HomeContentContainer } from 'components/atoms/containers/HomeContentContainer'
 import { PlayerTile } from 'components/atoms/PlayerTile'
 import { RoomSlugText } from 'components/atoms/RoomSlugText'
-import { SquareLoader } from 'react-spinners'
 import { designTokens } from 'styles/designTokens'
+import { Button, ButtonSizes } from 'components/atoms/Button'
 
 import { Props } from './types'
+import { PlayerTilesContainer } from './styles'
 
 const { color, space } = designTokens
 
@@ -12,23 +17,34 @@ const LobbyPageContent = ({ room, player, players }: Props) => {
     if (!currPlayer)
         return <SquareLoader color={color.black} loading size={space.lg} />
     return (
-        <>
+        <HomeContentContainer>
             <RoomSlugText slug={room.slug} />
-            <PlayerTile
-                name={currPlayer?.name}
-                isOffline={!currPlayer.isActive}
-            />
-            {players
-                .filter((p) => p.id !== player.id)
-                .map((p) => (
-                    <PlayerTile
-                        key={`${p.name}${p.id}`}
-                        name={p.name}
-                        noBorderTop
-                        isOffline={!p.isActive}
-                    />
-                ))}
-        </>
+            <PlayerTilesContainer>
+                <PlayerTile
+                    name={currPlayer?.name}
+                    isOffline={!currPlayer.isActive}
+                />
+                {players
+                    .filter((p) => p.id !== player.id)
+                    .map((p) => (
+                        <PlayerTile
+                            key={`${p.name}${p.id}`}
+                            name={p.name}
+                            noBorderTop
+                            isOffline={!p.isActive}
+                        />
+                    ))}
+            </PlayerTilesContainer>
+            {player.role === Role.ADMIN && (
+                <Button
+                    text="Start"
+                    size={ButtonSizes.md}
+                    onClick={() => {
+                        console.log('Ready!')
+                    }}
+                />
+            )}
+        </HomeContentContainer>
     )
 }
 
---
+++ components/templates/LobbyPageContent/styles.ts
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled'
+
+export const PlayerTilesContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`
