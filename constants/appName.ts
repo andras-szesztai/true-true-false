2a6c4642708cfe6badb3ed5e17b3ca227d1c35a7
András Szesztai <andras.szesztai@nielsen.com>
2022-08-14 18:02:27 +0200

    update NextSeo props

--- constants/appName.ts
+++ constants/appName.ts
@@ -1,1 +1,4 @@
 export const APP_NAME = 'TrueTrueFalse'
+
+export const APP_DESCRIPTION =
+    'An online team-building game which helps you find out more about your fellow players while having fun together.'
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import { Link, LinkSizes } from 'components/atoms/Link'
 import { MainTitle } from 'components/atoms/MainTitle'
 import { JoinRoom } from 'components/organisms/JoinRoom'
 import { RulesModal } from 'components/molecules/RulesModal'
-import { APP_NAME } from 'constants/appName'
+import { APP_DESCRIPTION, APP_NAME } from 'constants/appName'
 
 // MISC
 // - Review rules
@@ -17,19 +17,17 @@ const Home = () => (
     <>
         <NextSeo
             title={APP_NAME}
-            description="An online team building game, find out more about your fellow players while having fun together."
+            description={APP_DESCRIPTION}
             openGraph={{
                 type: 'website',
                 url: 'https://truetruefalse.netlify.app/',
-                title: APP_NAME,
-                description:
-                    'Online team building game, find out more about your fellow players while having fun together.',
-                images: [{ url: 'https://ibb.co/4Tj1xjQ' }],
-            }}
-            twitter={{
-                handle: '@handle',
-                site: '@some',
-                cardType: 'summary_large_image',
+                title: `${APP_NAME}, an online team building game`,
+                description: APP_DESCRIPTION,
+                images: [
+                    {
+                        url: 'https://i.ibb.co/Kq9KH91/Screenshot-2022-08-14-at-17-39-47.png',
+                    },
+                ],
             }}
         />
         <HomePageContainer>
