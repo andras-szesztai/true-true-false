7ecbf2aa4101ac2398af7e6b27229f799a325f6f
András Szesztai <andras.szesztai@nielsen.com>
2022-08-14 17:03:29 +0200

    style: make become admin button not fixed on small screen

--- components/molecules/BecomeAdminButton/styles.ts
+++ components/molecules/BecomeAdminButton/styles.ts
@@ -2,10 +2,24 @@ import styled from '@emotion/styled'
 
 import { designTokens } from 'styles/designTokens'
 
-const { space } = designTokens
+const { space, breakPoints } = designTokens
+
+const containerStylesMap = {
+    position: {
+        [breakPoints.md]: `${space.md}px`,
+        [breakPoints.lg]: `${space.lg}px`,
+    },
+}
 
 export const Container = styled.div`
-    position: fixed;
-    bottom: ${space.lg}px;
-    right: ${space.lg}px;
+    @media only screen and (min-width: ${breakPoints.md}px) {
+        position: fixed;
+        right: ${containerStylesMap.position[breakPoints.md]};
+        bottom: ${containerStylesMap.position[breakPoints.md]};
+    }
+
+    @media only screen and (min-width: ${breakPoints.lg}px) {
+        right: ${containerStylesMap.position[breakPoints.lg]};
+        bottom: ${containerStylesMap.position[breakPoints.lg]};
+    }
 `
